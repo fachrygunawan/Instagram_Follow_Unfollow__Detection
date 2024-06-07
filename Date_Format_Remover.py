2c007fb3fcfca7a019422addfde25fe79088dc2e
Muhammad Fachry Gunawan <fahrigilo991@gmail.com>
2024-06-07 16:48:02 +0700

    Update Date_Format_Remover.py

--- Date_Format_Remover.py
+++ Date_Format_Remover.py
@@ -1,10 +1,8 @@
 import re
 
 def remove_date_formats(text):
-    # Define the regex pattern to match the date format
     date_pattern = r'[A-Z][a-z]{2} \d{1,2}, \d{4}, \d{1,2}:\d{2} [AP]M'
 
-    # Use re.sub to replace all occurrences of the date format with an empty string
     cleaned_text = re.sub(date_pattern, '', text)
 
     return cleaned_text
